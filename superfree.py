dadb7c33ec3b9c6c090f7fa64e29dbc8dfd409ea
Reynold Chery <jfltech@gmail.com>
2013-03-17 23:54:14 -0400

    Initial Commit

---
+++ superfree.py
@@ -0,0 +1,18 @@
+import os 
+#import inotify
+
+#on startup scan src dir if exists
+#scan cache dir 
+SRC_DIR = {}
+SRC_DIR['/superfree'] = ['movies','tv_series']
+CACHE_DIR = '/superfree-cache'
+
+def scanDirectories():
+    for dirName, subdirList, fileList in os.walk(SRC_DIR.keys()):
+        
+        
+
+
+if "__name__" == "__main__":
+    pass
+    #inotify wait here..
