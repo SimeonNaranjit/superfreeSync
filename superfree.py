0752e403b4888f0558e2e3793617ff82570ac30f
junya <junya@charlie.(none)>
2013-03-18 19:58:34 -0400

    update sync function

--- superfree.py
+++ superfree.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os 
 #import inotify
 
@@ -5,15 +7,30 @@ import os
 #scan cache dir 
 SRC_DIR = {}
 SRC_DIR['/superfree'] = ['movies','tv_series']
-CACHE_DIR = '/superfree-cache'
+#CACHE_DIR = '/superfree-cache'
+CACHE_DIR = '/home/junya'
+TIME_IN_DAYS = 30
 
 def scanDirectories():
-    for dirName, subdirList, fileList in os.walk(SRC_DIR.keys()):
-        pass
-        
+    sourceDir = SRC_DIR.keys()[0]
+    for root, dirs, files in os.walk(CACHE_DIR):
+        dirs[:] = SRC_DIR['/superfree']
+        #skip hidden directories
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+        if os.path.exists(root.replace(sourceDir,CACHE_DIR,1))
+            pass
+            print('exists')
+        else:
+            if os.path.isfile(root):
+                f = os.stat(root)
+                f.st_atime(root) > TIME_IN_DAYS * 86400
+            rsyncFile(sourceDir,CACHE_DIR)
+
+        print(root)
+         
         
 
 
-if "__name__" == "__main__":
-    pass
+if __name__ == "__main__":
+    scanDirectories()
     #inotify wait here..
