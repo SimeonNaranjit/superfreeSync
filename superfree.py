33790c047229f882345955be5a95d9a6411109b0
Reynold Chery <jfltech@gmail.com>
2013-04-07 00:34:01 -0400

    some changes

--- superfree.py
+++ superfree.py
@@ -2,14 +2,14 @@
 
 import os 
 import subprocess
+import re
 #import inotify
 
 #on startup scan src dir if exists
 #scan cache dir 
 SRC_DIR = {}
-SRC_DIR['/superfree'] = ['movies','tv_series']
-#CACHE_DIR = '/superfree-cache'
-CACHE_DIR = '/home/junya'
+SRC_DIR['/mnt/superfree'] = ['movies','tv_series']
+CACHE_DIR = '/mnt/superfree-cache'
 TIME_IN_DAYS = 30
 FILE_PERCENT = 10
 
@@ -30,11 +30,38 @@ def scanDirectories():
             rsyncFile(sourceDir,CACHE_DIR)
 
         print(root)
+
+def getFileDuration(filename):
+    ffmpeg = "ffmpeg -i"
+    ffmpegOut = subprocess.check_output([ffmpeg,filename],shell=True)
+    for line in ffmpegOut.split('\n'):
+        match = re.search('[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9]',ffmpegOut)
+        if match:
+            duration = match.group()
+            return duration
+
+def getSec(s):
+    l = s.split(':')
+        return int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2])
+
+def getTime(secomds):
+    
+
+def getFilePercentages(duration):
+    seconds = getSec(duration)
+    if seconds > 300:
+        firstLength = 300:
+    if seconds > 1200:
+        sencondLength = 
+        
+
+        
          
-def copyFilePercent(fileName):
-    fileSize = os.path.getsize(fileName)
+def copyFilePercent(filename):
+    fileSize = os.path.getsize(filename)
     filePercentSize = fileSize * float("." + FILE_PERCENT)
-    subprocess.call(head --bytes=%s %s % filePercentSize
+    duration = getFileDuration(filename)
+    subprocess.(head --bytes=%s %s % filePercentSize
     
     
 if __name__ == "__main__":
